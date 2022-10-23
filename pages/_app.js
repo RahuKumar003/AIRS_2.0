42e73bde75792e8b2367361b68927e35d5152e3d
YTW7 <myahya96@gmail.com>
2022-10-23 19:59:23 +0530

    Revert "Added navbar component and created other pages..."
    This reverts commit 00e8c236eca080e24644f0dfa4a7b61c48182d76.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,7 @@
 import '../styles/globals.css'
-import { useState } from 'react'
-import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  const [isDark, setIsDark] = useState(false)
-  return <>
-  <style jsx global>{`
-  body {
-    background: ${isDark ? "black" : "black"};
-  }
-`}</style>
-  <Navbar/>
-  <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
